717bc77f7f789f2c23e48c042e74a1b06685b263
Angelo Amenta <angeloamenta7@gmail.com>
2026-02-20 15:19:04 +0100

    update johnny

--- src/components/ui/SplineMascot.jsx
+++ src/components/ui/SplineMascot.jsx
@@ -3,8 +3,11 @@ import Spline from '@splinetool/react-spline';
 export default function SplineMascot() {
     return (
         <div className="w-full h-full min-h-[500px] flex items-center justify-center">
-            <Spline
+            {/* <Spline
                 scene="https://prod.spline.design/VzT2GVZSUnwxNkcR/scene.splinecode"
+            /> */}
+            <Spline
+                scene="https://prod.spline.design/95YV0-SvNokzBgy2/scene.splinecode"
             />
         </div>
     );
